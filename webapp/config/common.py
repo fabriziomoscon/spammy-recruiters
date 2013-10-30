411d3222865f7f33abb046469bca499cc6eb3b96
Ian Crowther <ian@iancrowther.co.uk>
2013-10-30 23:57:49 +0000

    removed random app

--- webapp/config/common.py
+++
@@ -1,4 +0,0 @@
-# no debugging by default - this is overriden in run.py for local dev
-DEBUG = False
-# production DB
-SQLALCHEMY_DATABASE_URI = 'postgresql://spamsub:@localhost/spamsub'
--- webapp/run.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-sys.path.append("..")
-
-from webapp import app
-app.run(host='127.0.0.1')
